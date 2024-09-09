24cbced94a99aa4e7ebe37be2b7078ac55ba32d8
ltadams0311 <ltadams@crimson.ua.edu>
2024-09-08 23:50:18 -0500

    Lab 2 Submission

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    static void Main()
+    {
+        const double sandwichCost = 4.75;
+        const double toppingCost = 0.55;
+        const double discount = 0.10;
+
+        Console.Write("How many sandwiches? ");
+        int numberOfSandwiches = int.Parse(Console.ReadLine());
+
+        Console.Write("How many total toppings? ");
+        int numberOfToppings = int.Parse(Console.ReadLine());
+
+        Console.Write("What is the tip? (Write in decimal form) ");
+        double tipPercentage = double.Parse(Console.ReadLine());
+
+        double totalSandwichCost = numberOfSandwiches * sandwichCost;
+        double totalToppingCost = numberOfToppings * toppingCost;
+        double baseCost = totalSandwichCost + totalToppingCost;
+        double discountAmount = baseCost * discount;
+        double discountedCost = baseCost - discountAmount;
+        double tipAmount = discountedCost * tipPercentage;
+        double finalOrderCost = discountedCost + tipAmount;
+
+        Console.WriteLine($"Total Order Cost: ${finalOrderCost:F2}");
+    }
+}
